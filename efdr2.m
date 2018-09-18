f40986d126aec18f0fe5ac53cf320e2e32657cdb
Saurin Parikh <dr.saurin.parikh@gmail.com>
2018-09-18 10:55:52 -0400

    alternate efdr
    FDR(p) = (false positives based on p-value)/(total significants for that p-value)

---
+++ efdr2.m
@@ -0,0 +1,51 @@
+%%  Sau MATLAB Colony Analyzer Toolkit
+%
+%%  efdr2.m
+
+%   Author: Saurin Parikh, August 2018
+%   dr.saurin.parikh@gmail.com
+%   
+%   FDR(p) = (false positives based on p-value)/
+%               (total significants for that p-value)
+
+%%
+    function out = efdr2(tablename_fit,tablename_pval,hours,contname,t)
+    
+        connectSQL;        
+        out = [];
+        dat = [];
+        
+        for iii = 1:length(hours)       
+            query = sprintf(['select fitness from %s ',...
+                'where hours = %d and orf_name = ''%s'' ',...
+                'and fitness is not null'],tablename_fit,hours(iii),contname);
+            fitdat = fetch(conn, query);
+
+            dat = [];
+            for P = 0.01:0.01:0.05
+                
+                n = fetch(conn, sprintf(['select count(*) from %s ',...
+                    'where hours = %d and p <= %f'],...
+                    tablename_pval,hours(iii),P));
+                N = fetch(conn, sprintf(['select count(*) from %s ',...
+                    'where hours = %d'],...
+                    tablename_pval,hours(iii)));               
+                
+                cnt = 0;
+                for i = 1:N.count___
+                    [temp, ~] = datasample(fitdat.fitness, t);
+                    [p, ~, ~] = ranksum(temp, fitdat.fitness, 'alpha', 0.05, 'tail', 'both', 'method', 'approximate');
+                    if p <= P
+                        cnt = cnt + 1;
+                    end
+                end
+                
+                dat = [dat; [P cnt/n.count___ ]];
+            end
+            
+            out = [out; [ones(length(dat),1)*hours(iii) dat]];
+        end      
+        conn(close);    
+    end
+    
+    
