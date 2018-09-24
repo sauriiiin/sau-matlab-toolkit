bd4f3c197af3df311dd713903121e04a1f80fdf6
Saurin Parikh <dr.saurin.parikh@gmail.com>
2018-09-24 12:33:29 -0400

    cleanup

--- pos2coor2strainid.m
+++ pos2coor2strainid.m
@@ -1,4 +1,9 @@
-%%  pos2coor2strainid_MS.m
+%%  Sau MATLAB Colony Analyzer Toolkitv
+%
+%%  pos2coor2strainid.m
+
+%   Author: Saurin Parikh, September 2018
+%   dr.saurin.parikh@gmail.com
 
 %%  INDICES
 
@@ -55,63 +60,66 @@
 
 %%  POSITIONS
 
-%   positions/plate
+%   plate384 positions/plate
     pos384_03    = col2grid(linspace(1,384,384));
     pos384_05    = col2grid(linspace(384+1,384*2,384));
     pos384_22    = col2grid(linspace(384*2+1,384*3,384));
     pos384_c1    = col2grid(linspace(384*3+1,384*4,384));
-%   positions+indices/plate
+%   plate384 positions+indices/plate
     plate384    = [[grid2row(pos384_03), grid2row(pos384_05),...
         grid2row(pos384_22), grid2row(pos384_c1)]; coor384]';
     
     exec(conn, ['create table PT2_pos2coor384 (pos int not null, 384plate int not null,'...
         ' 384row int not null, 384col int not null)']);
-    tablename = 'BF_pos2coor384';
+    tablename = 'PT2_pos2coor384';
     colnames = {'pos','384plate','384row','384col'};
     datainsert(conn,tablename,colnames,plate384);
 
-%   plate1536
+%   plate1536 positions/plate
     pos1536_1 = plategen(pos384_c1,pos384_03,pos384_05,pos384_22)+10000;
     pos1536_2 = plategen(pos384_22,pos384_c1,pos384_03,pos384_05)+20000;
     pos1536_3 = plategen(pos384_05,pos384_22,pos384_c1,pos384_03)+30000;
     pos1536_4 = plategen(pos384_03,pos384_05,pos384_22,pos384_c1)+40000;
-    
+%   plate1536 positions+indices/plate    
     plate1536   = [[grid2row(pos1536_1), grid2row(pos1536_2),...
         grid2row(pos1536_3), grid2row(pos1536_4)]; coor1536]';    
     
-    exec(conn, ['create table MS_pos2coor1536 (pos int not null, 1536plate int not null,'...
+    exec(conn, ['create table PT2_pos2coor1536 (pos int not null, 1536plate int not null,'...
         ' 1536row int not null, 1536col int not null)']);
-    tablename = 'MS_pos2coor1536';
+    tablename = 'PT2_pos2coor1536';
     colnames = {'pos','1536plate','1536row','1536col'};
     datainsert(conn,tablename,colnames,plate1536);    
     
-%   plate6144
-    pos6144 = plategen(pos1536_1,pos1536_2,pos1536_3,pos1536_4)+100000;
+%   plate6144 positions/plate
+    pos6144_1 = plategen(pos1536_1,pos1536_2,pos1536_3,pos1536_4)+100000;
+    pos6144_2 = plategen(pos1536_1,pos1536_2,pos1536_3,pos1536_4)+200000;
+    pos6144_3 = plategen(pos1536_1,pos1536_2,pos1536_3,pos1536_4)+300000;
+%   plate6144 positions+indices/plate    
+    plate6144   = [[grid2row(pos6144_1),grid2row(pos6144_2),...
+        grid2row(pos6144_3)]; coor6144]';
     
-    plate6144   = [grid2row(pos6144); coor6144]';
-    
-    exec(conn, ['create table MS_pos2coor6144 (pos int not null, 6144plate int not null,'...
+    exec(conn, ['create table PT2_pos2coor6144 (pos int not null, 6144plate int not null,'...
         ' 6144row int not null, 6144col int not null)']);
-    tablename = 'MS_pos2coor6144';
+    tablename = 'PT2_pos2coor6144';
     colnames = {'pos','6144plate','6144row','6144col'};
     datainsert(conn,tablename,colnames,plate6144);
    
 %%  STRAINS ALL
 
 %   plate384
-    strain384_3    = fetch(conn, ['select strain_id from MS_pos2strainid ',...
+    strain384_3    = fetch(conn, ['select strain_id from PT2_pos2strainid ',...
         'where pos between 1 and 384 order by pos asc ']);
     strain384_3 = col2grid(strain384_3.strain_id);
 
-    strain384_5    = fetch(conn, ['select strain_id from MS_pos2strainid ',...
+    strain384_5    = fetch(conn, ['select strain_id from PT2_pos2strainid ',...
         'where pos between 385 and 384*2 order by pos asc ']);
     strain384_5    = col2grid(strain384_5.strain_id);
     
-    strain384_22    = fetch(conn, ['select strain_id from MS_pos2strainid ',...
+    strain384_22    = fetch(conn, ['select strain_id from PT2_pos2strainid ',...
         'where pos between 384*2+1 and 384*3 order by pos asc ']);
     strain384_22   = col2grid(strain384_22.strain_id);
 
-    strain384_c    = fetch(conn, ['select strain_id from MS_pos2strainid ',...
+    strain384_c    = fetch(conn, ['select strain_id from PT2_pos2strainid ',...
         'where pos between 384*3+1 and 384*4 order by pos asc ']);
     strain384_c    = col2grid(strain384_c.strain_id);
 
@@ -124,16 +132,17 @@
     strain1536   = [plate1536(:,1)';[grid2row(strain1536_1), grid2row(strain1536_2),...
         grid2row(strain1536_3), grid2row(strain1536_4)]]';    
     
-    tablename = 'MS_pos2strainid';
+    tablename = 'PT2_pos2strainid';
     colnames = {'pos','strain_id'};
     datainsert(conn,tablename,colnames,strain1536);    
     
 %   plate6144
     strain6144 = plategen(strain1536_1,strain1536_2,strain1536_3,strain1536_4);
     
-    strain6144   = [plate6144(:,1)'; grid2row(strain6144)]';
+    strain6144   = [plate6144(:,1)'; [grid2row(strain6144),...
+        grid2row(strain6144),grid2row(strain6144)]]';
     
-    tablename = 'MS_pos2strainid';
+    tablename = 'PT2_pos2strainid';
     colnames = {'pos','strain_id'};
     datainsert(conn,tablename,colnames,strain6144);
     
