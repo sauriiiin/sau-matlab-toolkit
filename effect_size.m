ee7405b24b8ebf9eff565b142df133b54bd4ff05
saur1n <saur1n@users.noreply.github.com>
2019-02-13 09:12:40 -0500

    typo correction

--- effect_size.m
+++ effect_size.m
@@ -11,8 +11,8 @@
     function [es, n] = effect_size(cont_n, cont_avg, cont_std,...
         orf_n, orf_avg, orf_std, z_sig, z_pow)
     
-        std = (((cont_n-1)*(cont_std).^2 + (orf_n-1).*(orf_std).^2)/(cont_n+orf_n-2)).^(0.5);
-        es = abs(cont_avg-orf_avg)/std;
+        sd = (((cont_n-1)*(cont_std).^2 + (orf_n-1).*(orf_std).^2)/(cont_n+orf_n-2)).^(0.5);
+        es = abs(cont_avg-orf_avg)/sd;
         n = ceil((((z_sig+z_pow)/es).^2)*1.15);
     
     end
